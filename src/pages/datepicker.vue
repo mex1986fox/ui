ede2ff25292da6397fcadd6fbad68f2edf75f5bd
mex1986fox <mex1986fox@mail.ru>
2018-07-16 12:14:22 +0700

    поработаал с ui-datapicker

--- src/pages/datepicker.vue
+++ src/pages/datepicker.vue
@@ -2,12 +2,23 @@
     <layout>
         <div class="container">
             <div class="row">
+                <div class="col_12 col-phone_6">
+                    <h1 class="ui-header ui-header_1"> Датапикер </h1>
+                    <br/>
+                </div>
                 <div class="col_2">
                    <ui-datepicker
                         name="datepicker"
                         caption="Выбрать дату">
-
                    </ui-datepicker> 
+                </div>
+                <div class="col_12">
+                    <ui-code value='<ui-datepicker
+    name="datepicker"
+    caption="Выбрать дату">
+</ui-datepicker>'>
+
+                   </ui-code>
                 </div>
             </div>
         </div>
--- src/ui/ui-datepicker/ui-datepicker.vue
+++ src/ui/ui-datepicker/ui-datepicker.vue
@@ -1,29 +1,23 @@
 <template>
     <div class="ui-datepicker">
-        <ui-text
-            :name="dName"
-            :caption="dCaption"
-            :readonly="true"
-            :value="dSelectDate" @onFocus="isFocus" @onBlur="isBlur">
+        <ui-text :name="dName" :caption="dCaption" :readonly="true" :value="dSelectDate" @onFocus="isFocus" @onBlur="isBlur">
         </ui-text>
         <div class="ui-datepicker__arrow" @click="dFocus=!dFocus">
             <i class="fa fa-calendar" aria-hidden="true"></i>
         </div>
-        
 
         <transition name="ui-datepicker__menu">
             <div class="ui-datepicker__menu" v-if="dFocus">
-            <div class="ui-datepicker__header">
-                <div @click="clickLeft()" 
-                    class="ui-button ui-button_circle ui-button_circle_mini ui-button_flat ui-datepicker__button_left">
-                    <i class="fa fa-angle-left" aria-hidden="true"></i>
-                </div>   
-                <div @click="clickRight()" class="ui-button ui-button_circle ui-button_circle_mini ui-button_flat ui-datepicker__button_right">
-                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <div class="ui-datepicker__header">
+                    <div @click="clickLeft()" class="ui-button ui-button_circle ui-button_circle_mini ui-button_flat ui-datepicker__button_left">
+                        <i class="fa fa-angle-left" aria-hidden="true"></i>
+                    </div>
+                    <div @click="clickRight()" class="ui-button ui-button_circle ui-button_circle_mini ui-button_flat ui-datepicker__button_right">
+                        <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    </div>
+                    {{compMonth.name +" "+compYear}}
                 </div>
-                {{compMouths.name +"   "+compYear}}
-            </div>
-            <div class="ui-datepicker__day">
+                <div class="ui-datepicker__day">
                     Пн
                 </div>
                 <div class="ui-datepicker__day">
@@ -45,118 +39,154 @@
                     Вс
                 </div>
                 <div class="ui-datepicker__number-day ui-datepicker__number-day_flat" v-for="keyn in compNumberDay" :key="'n'+keyn">
-                    
+
                 </div>
                 <div @click="clickNumber(key)" class="ui-datepicker__number-day" v-for="key in compSumDays" :key="key">
                     {{key}}
                 </div>
+                <div class="ui-datepicker__footer">
+                    <button class="ui-button ui-button_flat ui-button_mini ui-datepicker__button" @click="dActiveMenyYear=true">
+                        Выбрать год
+                    </button>
+                </div>
+                <transition name="ui-datepicker__menu-yaer">
+                    <div class="ui-datepicker__menu-yaer" v-if="dActiveMenyYear">
+                        <ul class="ui-datepicker__menu-yaer__select">
+                            <li class="ui-datepicker__menu-yaer__option" v-for="key in 400" :key="key" @click="clickYear(dPresentYear-key+1)">
+                                {{dPresentYear-key+1}}
+                            </li>
+                        </ul>
+                    </div>
+                </transition>
 
             </div>
-    
-    
-    
-    
+
         </transition>
+
     </div>
 </template>
 <script>
 export default {
-    data(){
-        return{
-            dFocus:false,
-            dCaption: this.caption,
-            dName: this.name,
-            dSelectDate: "",
-            dDate: new Date(),
-            dMouths: [{id:"01",name:"Январь",leapDays:31,days:31},
-                        {id:"02",name:"Февраль",leapDays:29,days:28},
-                        {id:"03",name:"Март",leapDays:31,days:31},
-                        {id:"04",name:"Апрель",leapDays:30,days:30},
-                        {id:"05",name:"Май",leapDays:31,days:31},
-                        {id:"06",name:"Июнь",leapDays:30,days:30},
-                        {id:"07",name:"Июль",leapDays:31,days:31},
-                        {id:"08",name:"Август",leapDays:31,days:31},
-                        {id:"09",name:"Сентябрь",leapDays:30,days:30},
-                        {id:"10",name:"Октябрь",leapDays:31,days:31},
-                        {id:"11",name:"Ноябрь",leapDays:30,days:30},
-                        {id:"12",name:"Декабрь",leapDays:31,days:31}]
-            
-        }
+  data() {
+    return {
+      dPresentYear: new Date().getFullYear(),
+      dActiveMenyYear: false,
+      dFocus: false,
+      dCaption: this.caption,
+      dName: this.name,
+      dSelectDate: "",
+      dDate: new Date(),
+      dMonth: [
+        { id: "01", name: "Январь", leapDays: 31, days: 31 },
+        { id: "02", name: "Февраль", leapDays: 29, days: 28 },
+        { id: "03", name: "Март", leapDays: 31, days: 31 },
+        { id: "04", name: "Апрель", leapDays: 30, days: 30 },
+        { id: "05", name: "Май", leapDays: 31, days: 31 },
+        { id: "06", name: "Июнь", leapDays: 30, days: 30 },
+        { id: "07", name: "Июль", leapDays: 31, days: 31 },
+        { id: "08", name: "Август", leapDays: 31, days: 31 },
+        { id: "09", name: "Сентябрь", leapDays: 30, days: 30 },
+        { id: "10", name: "Октябрь", leapDays: 31, days: 31 },
+        { id: "11", name: "Ноябрь", leapDays: 30, days: 30 },
+        { id: "12", name: "Декабрь", leapDays: 31, days: 31 }
+      ]
+    };
+  },
+  props: {
+    name: {
+      type: String,
+      default: ""
     },
-    props:{
-        name:{
-            type:String,
-            default:""
-        },
-        caption:{
-            type:String,
-            default:""
-        }
+    caption: {
+      type: String,
+      default: ""
+    }
+  },
+  methods: {
+    isFocus() {
+      this.dFocus = true;
     },
-    methods:{
-        isFocus(){
-            this.dFocus=true
-        },
-        isBlur(){
-            // this.dFocus=false
-        },
-        //определяет количество дней в месяце с учетом високоснокго года
-        sumDays(year, mouths){
-           return this.year%4==0 ?
-            this.dMouths[mouths].leapDays : 
-            this.dMouths[mouths].days;
-        },
-        clickLeft(){
-            let prevMouth=this.dDate.getMonth()-1;
-            let prevYear=this.compYear;
-            if(prevMouth < 0){
-                prevMouth=11;
-                --prevYear;
-            } 
-            this.dDate = new Date(prevYear,prevMouth,this.sumDays(prevYear, prevMouth));
-        },
-        clickRight(){
-            let prevMouth=this.dDate.getMonth()+1;
-            let prevYear=this.compYear;
-            if(prevMouth == 12){
-                prevMouth=0;
-                ++prevYear;
-            } 
-            this.dDate = new Date(prevYear,prevMouth,this.sumDays(prevYear, prevMouth));
-        },
-        clickNumber(number){
-            this.dSelectDate=this.compYear+"-"+this.compMouths.id+"-"+(number>9?number:"0"+number);
-            this.dFocus=false;
-        }
+    isBlur() {
+      // this.dFocus=false
     },
-    computed:{
-        compMouths(){
-            return this.dMouths[this.dDate.getMonth()];
-        },
-        compYear(){
-            return this.dDate.getFullYear();
-        },
-        compDay(){
-            return this.dDate.getDate();
-        },
-        compSumDays(){
-           return this.sumDays(this.compYear, this.dDate.getMonth());
-        },
-        compNumberDay(){
-            //jghеделить предыдущий месяц
-            let prevMouth=this.dDate.getMonth()-1;
-            let prevYear=this.compYear;
-            if(prevMouth < 0){
-                prevMouth=11;
-                --prevYear;
-            } 
-            let nDay= new Date(prevYear,prevMouth,this.sumDays(prevYear, prevMouth)).getDay();
-            return nDay;
-
-        }
-
+    //определяет количество дней в месяце с учетом високоснокго года
+    sumDays(year, month) {
+      return this.year % 4 == 0
+        ? this.dMonth[month].leapDays
+        : this.dMonth[month].days;
     },
-    mounted() {
+    clickLeft() {
+      let prevMouth = this.dDate.getMonth() - 1;
+      let prevYear = this.compYear;
+      if (prevMouth < 0) {
+        prevMouth = 11;
+        --prevYear;
+      }
+      this.dDate = new Date(
+        prevYear,
+        prevMouth,
+        this.sumDays(prevYear, prevMouth)
+      );
+    },
+    clickRight() {
+      let prevMouth = this.dDate.getMonth() + 1;
+      let prevYear = this.compYear;
+      if (prevMouth == 12) {
+        prevMouth = 0;
+        ++prevYear;
+      }
+      this.dDate = new Date(
+        prevYear,
+        prevMouth,
+        this.sumDays(prevYear, prevMouth)
+      );
+    },
+    clickNumber(number) {
+      this.dSelectDate =
+        this.compYear +
+        "-" +
+        this.compMonth.id +
+        "-" +
+        (number > 9 ? number : "0" + number);
+      this.dFocus = false;
+    },
+    clickYear(yaer) {
+      this.dDate = new Date(
+        yaer + "-" + this.compMonth.id + "-" + this.compDay
+      );
+      this.dActiveMenyYear = false;
+    }
+  },
+  computed: {
+    compMonth() {
+      return this.dMonth[this.dDate.getMonth()];
+    },
+    compYear() {
+      return this.dDate.getFullYear();
+    },
+    compDay() {
+      return this.dDate.getDate();
+    },
+    compSumDays() {
+      return this.sumDays(this.compYear, this.dDate.getMonth());
+    },
+    compNumberDay() {
+      //jghеделить предыдущий месяц
+      let prevMouth = this.dDate.getMonth() - 1;
+      let prevYear = this.compYear;
+      if (prevMouth < 0) {
+        prevMouth = 11;
+        --prevYear;
+      }
+      let nDay = new Date(
+        prevYear,
+        prevMouth,
+        this.sumDays(prevYear, prevMouth)
+      ).getDay();
+      return nDay;
+    }
+  },
+  mounted() {
     window.addEventListener(
       "click",
       event => {
@@ -179,6 +209,6 @@ export default {
       },
       true
     );
-  },
-}
+  }
+};
 </script>
