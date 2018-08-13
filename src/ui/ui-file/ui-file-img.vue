099eca80ac71ab0731bd5ca87348537a40ef961d
mex1986fox <mex1986fox@mail.ru>
2018-08-13 21:29:48 +0700

    ...

--- src/ui/ui-file/ui-file-img.vue
+++ src/ui/ui-file/ui-file-img.vue
@@ -1,42 +1,50 @@
 <template>
-    <div class="ui-file-img">
-        <input class="ui-file-img__input"
-            ref="file"
-            type="file"
-            name="file"
-            multiple>
-        <div v-for="(val, key) in dFiles"
-            :key="key"
-            class="ui-file-img__file">
-            <div class="ui-file-img__file-icon">
-                <i class="fa fa-file-o"
-                    aria-hidden="true"></i>
-            </div>
-            <div class="ui-file-img__file-type">
-                {{val.type.split("/")[1]}}
-            </div>
-            <div class="ui-file-img__file-src">
-                {{val.name}}
-            </div>
-        </div>
+	<div class="ui-file-img"
+	     tabindex="0">
+		<input class="ui-file-img__input"
+		       ref="file"
+		       type="file"
+		       name="file"
+		       multiple>
+		<span class="ui-file-img__caption"
+		      :class="{ 'ui-file-img__caption_completed':modCompleted}">
+			{{captionCompleted!="" && modCompleted?captionCompleted:caption}}
+		</span>
+		<div class="ui-file-img__files">
+			<div class="ui-file-img__files-file"
+			     v-for="(val, key) in dFiles"
+			     :key="key">
+				<div class="ui-file-img__files-icon">
+					<i class="fa fa-file-o"
+					   aria-hidden="true"></i>
+				</div>
+				<div class="ui-file-img__files-type">
+					{{val.type.split("/")[1]}}
+				</div>
+				<div class="ui-file-img__files-src">
+					{{val.name}}
+				</div>
+			</div>
 
-        <div class="ui-file-img__buttons">
-            <button v-if="dFiles.length==0"
-                @click="isClickAddFile"
-                class="ui-button ui-button_circle ui-button_mini ui-button_flat">
-                <i class="fa fa-plus"
-                    aria-hidden="true"></i>
+		</div>
 
-            </button>
-            <button v-if="dFiles.length>0"
-                @click="isClickAddFile"
-                class="ui-button ui-button_circle ui-button_circle__mini ui-button_flat">
-                <i class="fa fa-pencil"
-                    aria-hidden="true"></i>
-            </button>
-        </div>
-        
-    </div>
+		<hr class="ui-file-img__border">
+		<div class="ui-file-img__buttons">
+			<button v-if="dFiles.length==0"
+			        @click="isClickAddFile"
+			        class="ui-button ui-button_circle ui-button_circle_mini">
+				<i class="fa fa-plus"
+				   aria-hidden="true"></i>
+
+			</button>
+			<button v-if="dFiles.length>0"
+			        @click="isClickAddFile"
+			        class="ui-button ui-button_circle ui-button_circle_mini">
+				<i class="fa fa-pencil"
+				   aria-hidden="true"></i>
+			</button>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -45,11 +53,40 @@ export default {
   data() {
     return {
       dFiles: [],
-      dPath: undefined
+      dPath: undefined,
+      modCompleted: false
     };
   },
+  props: {
+    name: {
+      type: String,
+      default: ""
+    },
+    caption: {
+      type: String,
+      default: ""
+    },
+    caption: {
+      type: String,
+      default: ""
+    },
+    captionCompleted: {
+      type: String,
+      default: ""
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    help: {
+      type: String,
+      default: ""
+    }
+  },
+
   methods: {
     isClickAddFile() {
+      this.$el.focus();
       this.$refs.file.click();
     },
     isClickDelFile(number) {}
@@ -67,6 +104,9 @@ export default {
       this.dPath = newstring;
       this.dFiles = [];
       this.dFiles = event.target.files;
+      if (this.dFiles.length > 0) {
+        this.modCompleted = true;
+      }
     });
   }
 };
--- src/wg/wg-forms/wg-form-add.vue
+++ src/wg/wg-forms/wg-form-add.vue
@@ -1,170 +1,174 @@
 <template>
-    <div class="wg-form-add">
-        <div class="row">
-            <div class="col-12">
-                <div class="wg-form-add__bar">
-                    <div class="ui-header ui-header_2 wg-form-add__header">
-                        <span>Форма подачи объявления</span>
-                    </div>
-                    <button class="ui-button ui-button_circle ui-button_flat wg-form-add__close"
-                        @click="isClose">
-                        <i class="fa fa-angle-up"></i>
-                    </button>
-                    <ui-tabs class=" wg-form-add__tabs">
-                        <ui-tabs-tab id="basic"
-                            :checked="tabs=='basick'"
-                            @onFocus="isTabs">
-                            Основное
-                        </ui-tabs-tab>
-                        <ui-tabs-tab id="photo"
-                            :checked="tabs=='photo'"
-                            @onFocus="isTabs">
-                            Фотографии
-                        </ui-tabs-tab>
-                        <ui-tabs-tab id="excess"
-                            :checked="tabs=='excess'"
-                            @onFocus="isTabs">
-                            Дополнительно
-                        </ui-tabs-tab>
-                    </ui-tabs>
+	<div class="wg-form-add">
+		<div class="row">
+			<div class="col-12">
+				<div class="wg-form-add__bar">
+					<div class="ui-header ui-header_2 wg-form-add__header">
+						<span>Форма подачи объявления</span>
+					</div>
+					<button class="ui-button ui-button_circle ui-button_flat wg-form-add__close"
+					        @click="isClose">
+						<i class="fa fa-angle-up"></i>
+					</button>
+					<ui-tabs class=" wg-form-add__tabs">
+						<ui-tabs-tab id="basic"
+						             :checked="tabs=='basick'"
+						             @onFocus="isTabs">
+							Основное
+						</ui-tabs-tab>
+						<ui-tabs-tab id="photo"
+						             :checked="tabs=='photo'"
+						             @onFocus="isTabs">
+							Фотографии
+						</ui-tabs-tab>
+						<ui-tabs-tab id="excess"
+						             :checked="tabs=='excess'"
+						             @onFocus="isTabs">
+							Дополнительно
+						</ui-tabs-tab>
+					</ui-tabs>
 
-                </div>
+				</div>
 
-                <div class="wg-form-add__content"
-                    v-if="tabs=='basic'">
+				<div class="wg-form-add__content"
+				     v-show="tabs=='basic'">
 
-                    <div class="wg-form-add__hr">
-                        <i class="fa fa-map-marker"
-                            aria-hidden="true"></i>
-                        Местоположение
-                    </div>
-                    <div class="row">
-                        <div class="col_5">
-                            <ui-select name="region"
-                                caption="Субъект"
-                                :menu='[{value:"1",option:"Кемеровская", group:"Область", selected:true},
+					<div class="wg-form-add__hr">
+						<i class="fa fa-map-marker"
+						   aria-hidden="true"></i>
+						Местоположение
+					</div>
+					<div class="row">
+						<div class="col_5">
+							<ui-select name="region"
+							           caption="Субъект"
+							           :menu='[{value:"1",option:"Кемеровская", group:"Область", selected:true},
                                         {value:"2",option:"Новосибирская", group:"Область", selected:false},
                                         {value:"3",option:"Омская", group:"Области", selected:false},
                                         {value:"4",option:"Алтайский", group:"Край",selected:false},
                                         {value:"5",option:"Камчатский", group:"Край",selected:false}]'>
-                            </ui-select>
-                        </div>
-                        <div class="col_5 col_offset-2">
-                            <ui-select name="city"
-                                caption="Город"
-                                :multiple="true"
-                                :menu='[{value:"1",option:"Анжеро-Судженск", group:"Город", selected:true},
+							</ui-select>
+						</div>
+						<div class="col_5 col_offset-2">
+							<ui-select name="city"
+							           caption="Город"
+							           :multiple="true"
+							           :menu='[{value:"1",option:"Анжеро-Судженск", group:"Город", selected:true},
                                         {value:"2",option:"Новосибирск", group:"Город", selected:false},
                                         {value:"3",option:"Кемерово", group:"Город", selected:false},
                                         {value:"4",option:"Алтай", group:"Город",selected:false},
                                         {value:"5",option:"Яя", group:"Поселок",selected:false}]'>
-                            </ui-select>
-                        </div>
-                    </div>
+							</ui-select>
+						</div>
+					</div>
 
-                    <div class="wg-form-add__hr">
-                        <i class="fa fa-car"
-                            aria-hidden="true"></i>
+					<div class="wg-form-add__hr">
+						<i class="fa fa-car"
+						   aria-hidden="true"></i>
 
-                        Автомобиль
-                    </div>
-                    <div class="row">
-                        <div class="col_5">
-                            <ui-select name="marca"
-                                caption="Марка"
-                                :menu='[{value:"1",option:"Audi", group:"Область", selected:true},
+						Автомобиль
+					</div>
+					<div class="row">
+						<div class="col_5">
+							<ui-select name="marca"
+							           caption="Марка"
+							           :menu='[{value:"1",option:"Audi", group:"Область", selected:true},
                                         {value:"2",option:"Новосибирская", group:"Область", selected:false},
                                         {value:"3",option:"Омская", group:"Области", selected:false},
                                         {value:"4",option:"Алтайский", group:"Край",selected:false},
                                         {value:"5",option:"Камчатский", group:"Край",selected:false}]'>
-                            </ui-select>
-                        </div>
-                        <div class="col_5 col_offset-2">
-                            <ui-select name="model"
-                                caption="Модель"
-                                :multiple="true"
-                                :menu='[{value:"1",option:"A-150", group:"Город", selected:true},
+							</ui-select>
+						</div>
+						<div class="col_5 col_offset-2">
+							<ui-select name="model"
+							           caption="Модель"
+							           :multiple="true"
+							           :menu='[{value:"1",option:"A-150", group:"Город", selected:true},
                                         {value:"2",option:"Новосибирск", group:"Город", selected:false},
                                         {value:"3",option:"Кемерово", group:"Город", selected:false},
                                         {value:"4",option:"Алтай", group:"Город",selected:false},
                                         {value:"5",option:"Яя", group:"Поселок",selected:false}]'>
-                            </ui-select>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col_5">
-                            <ui-text caption="Пробег"></ui-text>
-                        </div>
-                    </div>
+							</ui-select>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col_5">
+							<ui-text caption="Пробег"></ui-text>
+						</div>
+					</div>
 
-                    <ui-textarea name="textarea1"
-                        caption="Описание"
-                        :autoresize="250"
-                        :height="100">
-                    </ui-textarea>
-                    <div class="wg-form-add__hr">
-                        <i class="fa fa-exclamation"
-                            aria-hidden="true"></i>
-                        Особые отметки
-                    </div>
-                    <div class="wg-form-add__checkboxs">
-                        <ui-check-box name="checkbox1"
-                            value="1"
-                            :checked="true">
-                            без документов
-                        </ui-check-box>
-                        <ui-check-box name="checkbox1"
-                            value="1"
-                            :checked="true">
-                            требует ремонта
-                        </ui-check-box>
-                        <ui-check-box name="checkbox1"
-                            value="1"
-                            :checked="true">
-                            готов к обмену
-                        </ui-check-box>
-                    </div>
+					<ui-textarea name="textarea1"
+					             caption="Описание"
+					             :autoresize="250"
+					             :height="100">
+					</ui-textarea>
+					<div class="wg-form-add__hr">
+						<i class="fa fa-exclamation"
+						   aria-hidden="true"></i>
+						Особые отметки
+					</div>
+					<div class="wg-form-add__checkboxs">
+						<ui-check-box name="checkbox1"
+						              value="1"
+						              :checked="true">
+							без документов
+						</ui-check-box>
+						<ui-check-box name="checkbox1"
+						              value="1"
+						              :checked="true">
+							требует ремонта
+						</ui-check-box>
+						<ui-check-box name="checkbox1"
+						              value="1"
+						              :checked="true">
+							готов к обмену
+						</ui-check-box>
+					</div>
 
-                    <div class="wg-form-add__hr">
-                        <i class="fa fa-phone"
-                            aria-hidden="true"></i>
-                        Связь
-                    </div>
-                    <div class="row">
-                        <div class="col_5">
-                            <ui-text caption="Телефон"></ui-text>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col_5">
-                            <ui-text caption="Цена"></ui-text>
-                        </div>
-                    </div>
-                </div>
-                <div class="wg-form-add__content"
-                    v-if="tabs=='photo'">
+					<div class="wg-form-add__hr">
+						<i class="fa fa-phone"
+						   aria-hidden="true"></i>
+						Связь
+					</div>
+					<div class="row">
+						<div class="col_5">
+							<ui-text caption="Телефон"></ui-text>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col_5">
+							<ui-text caption="Цена"></ui-text>
+						</div>
+					</div>
+				</div>
 
-                    <div class="wg-form-add__hr">
-                        <i class="fa fa-map-marker"
-                            aria-hidden="true"></i>
-                        Фотографии
-                    </div>
-                    <ui-file-img>
-                        
-                    </ui-file-img>
-                </div>
-                <div class="wg-form-add__buttons">
-                    <button class="ui-button ui-button_blue  ">
-                        Сохранить
-                    </button>
-                    <button class="ui-button ui-button_flat ">
-                        Отмена
-                    </button>
+				<div class="wg-form-add__content"
+				     v-show="tabs=='photo'">
 
-                </div>
-            </div>
-        </div>
-    </div>
+					<div class="wg-form-add__hr">
+						<i class="fa fa-camera"
+						   aria-hidden="true"></i>
+
+						Фотографии
+					</div>
+					<ui-file-img caption="Выберите фотографии"
+					             captionCompleted="Выбранные фотографии">
+
+					</ui-file-img>
+				</div>
+
+				<div class="wg-form-add__buttons">
+					<button class="ui-button ui-button_blue  ">
+						Сохранить
+					</button>
+					<button class="ui-button ui-button_flat ">
+						Отмена
+					</button>
+
+				</div>
+			</div>
+		</div>
+	</div>
 
 </template>
 
